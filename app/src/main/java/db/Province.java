f9938367e1478ad4bb86690891f803b95a88ea62
wj <wj@tfswufe.edu.cn>
2020-11-13 17:20:22 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/db/Province.java
@@ -0,0 +1,46 @@
+package db;
+
+import org.litepal.exceptions.DataSupportException;
+
+public class Province extends DataSupportException {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Province{" +
+                "id=" + id +
+                ", provinceName='" + provinceName + '\'' +
+                ", provinceCode=" + provinceCode +
+                '}';
+    }
+
+    public Province(String errorMessage) {
+        super(errorMessage);
+    }
+}
